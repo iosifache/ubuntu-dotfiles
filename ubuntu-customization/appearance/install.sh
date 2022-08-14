249990b5d391f247478340ca9dc2d2b000604bd1
George-Andrei Iosif <georgeiosif@live.com>
2022-08-14 19:19:21 +0300

    Downloads and set a profile image
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

--- ubuntu-customization/appearance/install.sh
+++ ubuntu-customization/appearance/install.sh
@@ -6,23 +6,32 @@ configure_dock() {
 	gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 	log_info "Set Dock position to bottom"
 	gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
-	log_info "Hidden 'Show Applications' icon in Dock"
+	log_info "Hidden "Show Applications" icon in Dock"
 	gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor
 	log_info "Set Dock to appear on each monitor"
 }
 
 configure_desktop() {
+	gsettings set org.gnome.desktop.background picture-uri "file://"$HOME"/Documents/Resources/dotfiles/ubuntu-customization/appearance/background.png"
+	log_info "Set desktop image"
 	gsettings set org.gnome.shell.extensions.ding show-home false
 	log_info "Hidden Home directory from desktop"
+
 }
 
 configure_nautilus() {
-	gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+	gsettings set org.gnome.nautilus.preferences default-folder-viewer "list-view"
 	log_info "Set default view as list in Nautilus"
+}
+
+configure_profile()  {
+	wget $PROFILE_IMAGE -O $HOME/.face
+	log_info "Downloaded and set profile image"
 }
 
 configure_file_system
 configure_dock
 configure_desktop
 configure_nautilus
+configure_profile
 log_info "Set operating system appearance"
