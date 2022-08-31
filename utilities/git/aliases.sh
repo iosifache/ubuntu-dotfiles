a6f982d7deb2d3505dabf3d27c14a1a77db75db7
George-Andrei Iosif <georgeiosif@live.com>
2022-08-31 10:17:13 +0300

    Adds new Git aliases
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

--- utilities/git/aliases.sh
+++ utilities/git/aliases.sh
@@ -1,7 +1,18 @@
 alias git-commit="git commit -s -m"
 alias git-status-all="git status -uall"
 alias git-push="git push origin"
+alias git-push-main="git push origin main"
+alias git-pull="git push origin"
+alias git-pull-main="git push origin main"
 alias git-add-all="git add -A"
+
+git-add-commit-push()  {
+    local message=$1
+    local branch=$2
+
+    git add --all
+    git-commit-push $message $branch
+}
 
 git-commit-push()  {
     local message=$1
@@ -24,4 +35,8 @@ git-amend-push()  {
     git add --all
     git commit --amend --no-edit
     git push --force origin $branch
+}
+
+git-discard-last-commit() {
+    git reset HEAD^
 }
