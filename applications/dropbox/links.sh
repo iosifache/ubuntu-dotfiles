aae62318118154c8a84dfdc302a7aca85e6d487a
George-Andrei Iosif <georgeiosif@live.com>
2022-09-25 16:32:12 +0300

    Adds Dropbox synced folders
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

---
+++ applications/dropbox/links.sh
@@ -0,0 +1,7 @@
+configure_links() {
+    ln -s $USER_HOME/Dropbox/Synced/KnowledgeBase $USER_HOME/Documents/Resources/Zettlekasten/KnowledgeBase
+    ln -s $USER_HOME/Dropbox/Synced/Documents $USER_HOME/Documents/Resources/Documents
+    ln -s $USER_HOME/Dropbox/Synced/Photos $USER_HOME/Documents/Resources/Photos
+}
+
+configure_links
